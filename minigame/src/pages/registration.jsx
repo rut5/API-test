595148d7ff4d9507197b76bb8bf4bbd4c16c000a
Rut <rutwintzell@outlook.com>
2025-12-02 13:28:11 +0100

    Started with registration

--- minigame/src/pages/registration.jsx
+++ minigame/src/pages/registration.jsx
@@ -8,20 +8,37 @@ import { useState, useEffect } from "react";
 
 function Registration() {
 
+    const [search, setSearch] = useState('');
+    const [list, setList] = useState([]); // empty array 
 
+    const addUser = () => {
+        localStorage.setItem('username', search);
+        setSearch('');
+        setList((l) => [...l, localStorage.getItem('username')]); // spread operator
+    };
+    useEffect(() => {
+        setList([localStorage.getItem('username')]);
+    }, []); // only runs once, renders the key value pairs in local storage
 
     return (
         <div>
             <h1>Registration</h1>
             <br />
-            <input type="text" placeholder="Username" />
-            <button>Done</button>
+
+            <div className="reg-form">
+                <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button onClick={addUser}>Register</button>
+
+                {/* adds a list */}
+                <ul>
+                    {list.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
             <br />
 
-
-            <input type="password" placeholder="Password" />
-            <br />
-            <button>Register</button>
         </div>
     );
 
